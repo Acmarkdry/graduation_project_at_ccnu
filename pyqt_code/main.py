c8a242f3516caaedc45ae79281b8f8d4d900376a
Administrator <1476571312@qq.com>
2022-02-15 19:03:02 +0800

    toolbar完成

--- pyqt_code/main.py
+++ pyqt_code/main.py
@@ -1,4 +1,8 @@
+import json
 import sys
+
+import numpy
+import pandas
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QMimeData
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog, QMessageBox, \
@@ -15,13 +19,17 @@ class Demo(QMainWindow):
     """
     def __init__(self):
         super(Demo, self).__init__()
+        self.recommand_result = [] # 推荐结果列表
         self.setWindowTitle("Recommand Resources Algorithm")
 
         #self.resources_bar = self.addToolBar('Resources')
         self.resize(800,1000)
         # 以上为各插件的初始化
 
-        self.init_ui()
+        self.menu_init()
+        self.toolbar_init()
+        self.status_bar_init()
+        self.action_init()
 
     def menu_init(self):
         self.resources_bar = self.addToolBar('Resources')
@@ -34,25 +42,115 @@ class Demo(QMainWindow):
 
         self.resources_action = QAction("Resources",self)
         self.teacher_action = QAction("Teacher",self)
-        self.knoledge_action = QAction("knowledge",self)
+        self.knowledge_action = QAction("knowledge",self)
         self.help_action = QAction("Help",self)
         self.save_action = QAction("Save",self)
 
-        self.set_centrt()
+        self.set_center()
 
     def set_center(self):
         # 将栏目居中
-        pass
+        #self.resize(QApplication.desktop().width() - 150,QApplication.desktop().height() - 150)
+
+        # desktop = QApplication.desktop()
+        # d_width = desktop.width()
+        # d_height = desktop.height()
+        #
+        # pos_x = d_width/2 - self.frameGeometry().width()/2
+        # pos_y = d_height/2 - self.frameGeometry().height()/2
+        #
+        # self.move(int(pos_x),int(pos_y))
+        self.resize(QApplication.desktop().width() - 2500, QApplication.desktop().height() - 150)
 
     def toolbar_init(self):
         # 初始化toolbar
-        pass
+        self.resources_bar.addAction(self.resources_action)
+        self.teacher_bar.addAction(self.teacher_action)
+        self.knowledge_bar.addAction(self.knowledge_action)
+        self.help_bar.addAction(self.help_action)
+        self.save_bar.addAction(self.save_action)
 
     def status_bar_init(self):
-        pass
+        self.status_bar.showMessage("Ready to compose")
 
     def action_init(self):
+        # self.add_player_action.setShortcut('Ctrl+A')
+        # self.add_player_action.setToolTip("Add a new player")## 设置气泡提示
+        # self.add_player_action.setStatusTip("Add a new player[Ctrl+A]") ## 设置状态栏提示
+        # self.add_player_action.triggered.connect(self.add_player) ## 设置触发器
+        self.resources_action.setShortcut('Ctrl+R')
+        self.resources_action.setToolTip("Add Resources")
+        self.resources_action.setStatusTip("Add your resources[Ctrl+R]")
+        self.resources_action.triggered.connect(self.add_resources)
+
+        self.teacher_action.setShortcut('Ctrl+T')
+        self.teacher_action.setToolTip('Add Teacher')
+        self.teacher_action.setStatusTip('Add your teacher[Ctrl+T]')
+        self.teacher_action.triggered.connect(self.add_teacher)
+
+        self.knowledge_action.setShortcut('Ctrl+K')
+        self.knowledge_action.setToolTip('Add knowledge reflection')
+        self.knowledge_action.setStatusTip('Add knowledge function[Ctrl+K]')
+        self.knowledge_action.triggered.connect(self.add_knowledge)
+
+        self.help_action.setShortcut('Ctrl+H')
+        self.help_action.setToolTip('Help you')
+        self.help_action.setStatusTip('Help you[Ctrl+H]')
+        self.help_action.triggered.connect(self.add_help)
+
+        self.save_action.setShortcut('Ctrl+S')
+        self.save_action.setToolTip('Save some file')
+        self.save_action.setStatusTip('Save some file[Ctrl+S]')
+        self.save_action.triggered.connect(self.add_save_function)
+
+    def add_resources(self):
+        # 打开json文件 对应resources文件夹
+        file,_ = QFileDialog.getOpenFileName(self,'open File','../','Files(*.json)')
+
+        if file:
+            f = open(file,'r')
+            content = f.read()
+            f.close()
+
+            self.student_resources = json.loads(content)
+        else :
+            pass
+
+    def add_teacher(self):
+        # 打开csv文件
+        file,_ = QFileDialog.getOpenFileName(self,'open File','../','Files(*.csv)')
+
+        if file:
+            self.teacher = pandas.read_csv(file)
+            self.teacher = numpy.array(self.teacher)
+
+        else:
+            pass
+
+    def add_knowledge(self):
+        # 打开csv文件
+        file,_ = QFileDialog.getOpenFileName(self,'Open File','../','Files(*.csv)')
+
+        if file:
+            self.knowledge_resources = pandas.read_csv(file)
+            self.knowledge_resources = numpy.array(self.knowledge_resources)
+
+    def add_help(self):
         pass
 
+    def add_save_function(self):
+        file_name,_ = QFileDialog.getSaveFileName(
+            self,'Save File','../','Files(*csv)'
+        )
+        if file_name:
+            tmp = pandas.DataFrame(data = self.recommand_result)
+            tmp.to_csv(file_name, encoding='utf-8', index=False)
+
+        else:
+            pass
+
 if __name__ == "__main__":
-    pass
+    app = QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
